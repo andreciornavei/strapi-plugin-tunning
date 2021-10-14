3ccc1438f61e3b43b890031aeee04c051c43ffda
account1 <andre.ciornavei@gmail.com>
2021-10-14 12:03:19 -0300

    fix route resolver

--- services/resolve_route.js
+++ services/resolve_route.js
@@ -5,14 +5,19 @@ module.exports = (ctx) => {
     // PARSE REQUESTED ROUTE TO ORIGINAL PATH //
     // ************************************** //
     let originalPath = ctx.request.url.split("?")[0]
-    if (ctx.params && Object.keys(ctx.params).length > 0) {
+    const params = ctx.params || {}
+    // remove the param "0" from route if it exists
+    // to prevent broken pregmactch logic replacer
+    if (params["0"]) delete params["0"]
+    if (Object.keys(params).length > 0) {
         originalPath = originalPath.split("/").map(partValue => {
-            for (const partKey in ctx.params) {
-                if (ctx.params[partKey] == partValue) return `(:.*\/|(:.*))`
+            for (const partKey in params) {
+                if (params[partKey] == partValue) return `(:.*\/|(:.*))`
             }
             return partValue
         }).join("\/")
     }
+
 
     // Get all application routes
     let routes = strapi.config.routes
